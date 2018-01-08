9142d14977be368e093af464c6b8b790113ef85b
Jonas <Jonas@JoN.Speedport_W_724V_09011603_05_010>
2018-01-08 19:09:01 +0100

    Created new package main, Started working on filechooser

--- FileRenamer/src/view/ViewController.java
+++ FileRenamer/src/view/ViewController.java
@@ -1,5 +1,29 @@
 package view;
 
+import java.io.File;
+
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import main.FileRenamerMain;
+
 public class ViewController {
 
+	private FileRenamerMain mainApp;
+	
+	@FXML
+	private void initialize(){
+		
+	}
+	
+	private void openFileChooser() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Resource File");
+		File chosenDir = fileChooser.showOpenDialog(null);
+				
+	}
+	
+	private void setMainApp(FileRenamerMain mainApp) {
+		this.mainApp = mainApp;
+	}
+	
 }
