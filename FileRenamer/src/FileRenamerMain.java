1ddb22724aada963a48b98ba5487f75138480893
Jonas <Jonas@JoN.Speedport_W_724V_09011603_05_010>
2018-01-08 18:49:43 +0100

    Added logger, Created fxml files

--- FileRenamer/src/FileRenamerMain.java
+++ FileRenamer/src/FileRenamerMain.java
@@ -1,8 +1,23 @@
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
 
 public class FileRenamerMain {
 
-	public static void main(String[] args) {
 
+	FileRenamerMain(){
+		File dir = new File("C:\\HTW\\1semester\\AngewMathe\\Übung\\renameTest");
+		List<File> files = (List<File>) FileUtils.listFiles(dir, TrueFileFilter.INSTANCE, null);
+		for(File f : files) {
+			Logger.log(f.getName());
+		}
+	}
+	
+	public static void main(String[] args) {
+		new FileRenamerMain();
 	}
 
 }
